07cb522f82e0704c27e5116bd686844a78307f16
Mendel van 't Riet <mendel@vantriet.nl>
2025-06-13 14:51:03 +0200

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,40 +1,81 @@
-function sayHello(yourName) {
-    if (yourName === undefined) {
-        console.log('Hello, no name')
-    } else {
-        console.log('Hello, ' + yourName)
+class Greeter {
+    constructor (name) {
+        this.name = name
+    }
+    getGreeting () {
+        if (this.name === undefined) {
+            return 'Hello, no name'
+        }
+        return 'Hello, ' + this.name
+    }
+    showGreeting (greetingMessage) {
+        console.log(greetingMessage)
+    }
+    greet () {
+        this.showGreeting(this.getGreeting())
     }
 }
+const g = new Greeter('Patchy')
+g.greet()
 
-const yourName = 'Your Name'
-// Put your name here
-console.log('Before setTimeout')
-setTimeout(() => {
-        sayHello(yourName)
-    }, 2000
-)
-
-console.log('After setTimeout')
-
-for(let i = 0; i < 10; i += 1) {
-    console.log('for loop i: ' + i)
+class DelayedGreeter extends Greeter {
+    delay = 2000
+    constructor (name, delay) {
+        super(name)
+        if (delay !== undefined) {
+            this.delay = delay
+        }
+    }
+    greet () {
+        setTimeout(
+            () => {
+                this.showGreeting(this.getGreeting())
+            }, this.delay
+        )
+    }
 }
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
 
-let j = 0
-while(j < 10) {
-    console.log('while loop j: ' + j)
-    j += 1
-}
-let k = 10
-do {
-    console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-numbers.forEach((value => {
-    console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-console.log('Here are the doubled numbers')
-console.log(doubled)
+//function sayHello(yourName) {
+//    if (yourName === undefined) {
+//        console.log('Hello, no name')
+//    } else {
+//        console.log('Hello, ' + yourName)
+//    }
+//}
+//
+//const yourName = 'Your Name'
+//// Put your name here
+//console.log('Before setTimeout')
+//setTimeout(() => {
+//        sayHello(yourName)
+//    }, 2000
+//)
+//
+//console.log('After setTimeout')
+//
+//for(let i = 0; i < 10; i += 1) {
+//    console.log('for loop i: ' + i)
+//}
+//
+//let j = 0
+//while(j < 10) {
+//    console.log('while loop j: ' + j)
+//    j += 1
+//}
+//let k = 10
+//do {
+//    console.log('do while k: ' + k)
+//} while(k < 10)
+//
+//const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//numbers.forEach((value => {
+//    console.log('For each value ' + value)
+//}))
+//
+//const doubled = numbers.map(value => value * 2)
+//console.log('Here are the doubled numbers')
+//console.log(doubled)
