aa43408ad35f21e81f1253218dec39d34b14a32e
Mendel van 't Riet <mendel@vantriet.nl>
2025-06-21 10:10:03 +0200

    Finish JSX

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,81 +1,80 @@
-class Greeter {
-    constructor (name) {
-        this.name = name
-    }
-    getGreeting () {
-        if (this.name === undefined) {
-            return 'Hello, no name'
+class PostRow extends React.Component {
+    render () {
+        const post = this.props.post
+        let thumbnail
+        if (post.hero_image.thumbnail) {
+            thumbnail = <img src={post.hero_image.thumbnail}/>
+        } else {
+            thumbnail = '-'
         }
-        return 'Hello, ' + this.name
-    }
-    showGreeting (greetingMessage) {
-        console.log(greetingMessage)
-    }
-    greet () {
-        this.showGreeting(this.getGreeting())
+        return <tr>
+            <td>{post.title}</td>
+            <td>{thumbnail}</td>
+            <td>{post.tags.join(', ')}</td>
+            <td>{post.slug}</td>
+            <td>{post.summary}</td>
+            <td><a href={'/post/' + post.slug + '/'}>View</a></td>
+        </tr>
     }
 }
-const g = new Greeter('Patchy')
-g.greet()
 
-class DelayedGreeter extends Greeter {
-    delay = 2000
-    constructor (name, delay) {
-        super(name)
-        if (delay !== undefined) {
-            this.delay = delay
+class PostTable extends React.Component {
+    state = {
+        dataLoaded: true,
+        data: {
+            results: [
+                {
+                    id: 15,
+                    tags: [
+                        'django', 'react'
+                    ],
+                    'hero_image': {
+                        'thumbnail': '/media/__sized__/hero_images/snake-419043_1920-thumbnail-100x100-70.jpg',
+                        'full_size': '/media/hero_images/snake-419043_1920.jpg'
+                    },
+                    title: 'Test Post',
+                    slug: 'test-post',
+                    summary: 'A test post, created for Django/React.'
+                }
+            ]
         }
     }
-    greet () {
-        setTimeout(
-            () => {
-                this.showGreeting(this.getGreeting())
-            }, this.delay
-        )
+    render () {
+        let rows
+        if (this.state.dataLoaded) {
+            if (this.state.data.results.length) {
+                rows = this.state.data.results.map(post => <PostRow
+                post={post} key={post.id}/>)
+            } else {
+                rows = <tr>
+                    <td colSpan="6">No results found.</td>
+                </tr>
+            }
+        } else {
+            rows = <tr>
+                <td colSpan="6">Loading&hellip;</td>
+            </tr>
+        }
+        return <table className="table table-striped table-bordered mt-2">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Slug</th>
+                <th>Summary</th>
+                <th>Link</th>
+            </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
     }
 }
-const dg2 = new DelayedGreeter('Patchy 2 Seconds')
-dg2.greet()
-const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
-dg1.greet()
 
-//function sayHello(yourName) {
-//    if (yourName === undefined) {
-//        console.log('Hello, no name')
-//    } else {
-//        console.log('Hello, ' + yourName)
-//    }
-//}
-//
-//const yourName = 'Your Name'
-//// Put your name here
-//console.log('Before setTimeout')
-//setTimeout(() => {
-//        sayHello(yourName)
-//    }, 2000
-//)
-//
-//console.log('After setTimeout')
-//
-//for(let i = 0; i < 10; i += 1) {
-//    console.log('for loop i: ' + i)
-//}
-//
-//let j = 0
-//while(j < 10) {
-//    console.log('while loop j: ' + j)
-//    j += 1
-//}
-//let k = 10
-//do {
-//    console.log('do while k: ' + k)
-//} while(k < 10)
-//
-//const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//numbers.forEach((value => {
-//    console.log('For each value ' + value)
-//}))
-//
-//const doubled = numbers.map(value => value * 2)
-//console.log('Here are the doubled numbers')
-//console.log(doubled)
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+    React.createElement(PostTable),
+    domContainer
+)
---
+++ blog/static/blog/jsx.js
@@ -0,0 +1,35 @@
+class ClickButton extends React.Component {
+    state = {
+        wasClicked: false
+    }
+
+    handleClick () {
+        this.setState(
+            {wasClicked: true}
+        )
+    }
+
+    render () {
+        let buttonText
+        if (this.state.wasClicked)
+            buttonText = 'Clicked!'
+        else
+            buttonText = 'Click Me'
+        return <button
+            className="btn btn-primary mt-2"
+            onClick={
+                () => {
+                    this.handleClick()
+                }
+            }
+        >
+            {buttonText}
+        </button>
+    }
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+    React.createElement(ClickButton),
+    domContainer
+)
